31b95bb99031a6af693a9af531d099db65fe508f
Ejiro <ejiroghenebenodiri@gmail.com>
2026-03-15 16:27:36 +0100

    .

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,66 +1,71 @@
 export const projects = [
-  {
+    {
     id: 1,
+    title: 'Rave Anti-Dandruff Shampoo',
+    image: '/myWork/Rave.jpeg'
+  },
+  {
+    id: 2,
     title: 'Black Modern Happy Birthday Poster',
     image: '/myWork/Black Modern Happy Birthday Poster.jpg'
   },
   {
-    id: 2,
+    id: 3,
     title: 'Expert Architectural Design',
     image: '/myWork/EXPERT ACHITECTURAL.png'
   },
   {
-    id: 3,
+    id: 4,
     title: 'Computer Service Repair Logo',
     image: '/myWork/Green and White Modern Computer Service Repair Logo.png'
   },
   {
-    id: 4,
+    id: 5,
     title: 'Lamborghini Design',
     image: '/myWork/Lamborghini.png'
   },
   {
-    id: 5,
+    id: 6,
     title: 'Creative Design Project 1',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.08.jpeg'
   },
   {
-    id: 6,
+    id: 8,
     title: 'Creative Design Project 2',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.09 (1).jpeg'
   },
   {
-    id: 7,
+    id: 9,
     title: 'Creative Design Project 3',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.09.jpeg'
   },
   {
-    id: 8,
+    id: 10,
     title: 'Creative Design Project 4',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.10 (1).jpeg'
   },
   {
-    id: 9,
+    id: 11,
     title: 'Creative Design Project 5',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.10.jpeg'
   },
   {
-    id: 10,
+    id: 12,
     title: 'Creative Design Project 6',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.11 (1).jpeg'
   },
   {
-    id: 11,
+    id: 13,
     title: 'Creative Design Project 7',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.11 (2).jpeg'
   },
   {
-    id: 12,
+    id: 14,
     title: 'Creative Design Project 8',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.11.jpeg'
   },
   {
-    id: 13,
+    id: 15,
     title: 'Creative Design Project 9',
     image: '/myWork/WhatsApp Image 2026-03-09 at 10.22.12.jpeg'
   }
